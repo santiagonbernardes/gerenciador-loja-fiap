e71808c6eca1cc799a321ea4bfa808c5653700f7
Santiago Bernardes <santiagobernardes@gmail.com>
2024-09-09 18:39:37 -0300

    checkpoint 3

--- app/classes/estoque.py
+++ app/classes/estoque.py
@@ -10,22 +10,22 @@ class Estoque:
         self.produtos_estocados: dict[int, int] = {}
         self.limite_estoque_baixo: int = limite_estoque_baixo
 
-    def adicionar(self, produto, quantidade) -> None:
+    def adicionar(self, produto: Produto, quantidade: int) -> None:
         self.execute_observando_limite_estoque_baixo(self.execute_adicionar, produto, quantidade)
 
-    def remover(self, produto, quantidade_remover) -> None:
+    def remover(self, produto: Produto, quantidade_remover: int) -> None:
         self.execute_observando_limite_estoque_baixo(self.execute_remover, produto, quantidade_remover)
 
-    def atualizar(self, produto, nova_quantidade) -> None:
+    def atualizar(self, produto: Produto, nova_quantidade: int) -> None:
         self.execute_observando_limite_estoque_baixo(self.execute_atualizar, produto, nova_quantidade)
 
-    def obtenha_quantidade_estocada(self, produto) -> int:
+    def obtenha_quantidade_estocada(self, produto: Produto) -> int:
         codigo_produto: int = produto.codigo
         if codigo_produto not in self.produtos_estocados:
             raise ProdutoNaoEstocadoException()
         return self.produtos_estocados[codigo_produto]
 
-    def atualize_limite_estoque_baixo(self, novo_limite) -> None:
+    def atualize_limite_estoque_baixo(self, novo_limite: int) -> None:
         self.limite_estoque_baixo = novo_limite
         for codigo_produto, quantidade in self.produtos_estocados.items():
             if quantidade < novo_limite:
@@ -42,14 +42,14 @@ class Estoque:
         if self.produtos_estocados[codigo_produto] < self.limite_estoque_baixo:
             self.mostre_mensagem_estoque_baixo(codigo_produto)
 
-    def execute_adicionar(self, produto: Produto, quantidade: int):
+    def execute_adicionar(self, produto: Produto, quantidade: int) -> None:
         codigo_produto: int = produto.codigo
         if codigo_produto in self.produtos_estocados:
             self.produtos_estocados[codigo_produto] += quantidade
         else:
             self.execute_atualizar(produto, quantidade)
 
-    def execute_remover(self, produto: Produto, quantidade_remover: int):
+    def execute_remover(self, produto: Produto, quantidade_remover: int) -> None:
         codigo_produto: int = produto.codigo
 
         if codigo_produto not in self.produtos_estocados:
@@ -60,10 +60,10 @@ class Estoque:
 
         self.produtos_estocados[codigo_produto] -= quantidade_remover
 
-    def execute_atualizar(self, produto: Produto, nova_quantidade: int):
+    def execute_atualizar(self, produto: Produto, nova_quantidade: int) -> None:
         codigo_produto: int = produto.codigo
         self.produtos_estocados[codigo_produto] = nova_quantidade
 
-    def mostre_mensagem_estoque_baixo(self, codigo_produto: int):
+    def mostre_mensagem_estoque_baixo(self, codigo_produto: int) -> None:
         estoque_atual: int = self.produtos_estocados[codigo_produto]
         print(f'Atenção: o produto#{codigo_produto} está com estoque baixo. Estoque atual é: {estoque_atual}.')
